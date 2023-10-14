2dc9de99c75c080cd49ad0826fd811b71a29c6f2
Aditya singh <akash.aditya001@gmail.com>
2023-10-14 15:21:02 +0530

    mba_2.0

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -1,6 +1,24 @@
-async function getAllTheatres(req, res) {}
-async function getTheatreById(req, res) {}
-async function createTheatre(req, res) {}
+const Theatre = require("../models/theatre.model")
+
+async function getAllTheatres(req, res) {
+const theaters = await Theatre.find();
+res.status(200).send(theaters);
+}    
+
+async function getTheatreById(req, res) {
+    const id = req.params.id;
+    try {
+        const theatre = await Theatre.findById(id);
+        res.status(200).send(theatre);
+    } catch (ex) {
+        res.status(404).send ({
+            message: `Theater with ID : ${id} does not exists`,
+        })
+    }
+    }
+async function createTheatre(req, res) {
+    
+}
 async function updateTheatre(req, res) {}
 async function deleteTheatre(req, res) {}
 async function addMoviesToATheatre(req, res) {}
@@ -11,4 +29,4 @@ module.exports = {
     updateTheatre,
     deleteTheatre,
     addMoviesToATheatre,
-};
+}
