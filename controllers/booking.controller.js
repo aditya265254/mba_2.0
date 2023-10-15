ce278debd5304d5b471d87229c3f3d6c051ce6ae
Aditya singh <akash.aditya001@gmail.com>
2023-10-15 13:18:33 +0530

    mba_2.0

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,7 +1,74 @@
-async function getAllBookings(req, res) {}
-async function getBookingById(req, res) {}
-async function updateBooking(req, res) {}
-async function createBooking(req, rea) {}
+const { USERTYPES } = require("../constant");
+const Booking = require("../models/booking.model");
+const User = require("../models/user.model");
+
+async function getAllBookings(req, res) {
+
+     
+    if (req.userType === USERTYPES.CUSTOMER) {
+        const userId = req.userId;
+        const user = await User.find({ userId: userId});
+        const queryObj = {};
+        queryObj.userId = user._id;
+    } 
+    const bokkings = await Booking.find(queryObj);
+    res.status(200).send(bookings);
+}
+async function getBookingById(req, res) {
+   try { 
+    const booking = await Booking.findById(req.params.id);
+    if (req.userType === USERTYPES.ADMIN || req.userType === USERTYPES.CLINT){
+        res.send(booking);
+    } else if (req.userType ===  USERTYPES.CUSTOMER) {
+        const userId = req.userId;
+        const user = await User.find({ userId: userId});
+         
+        if (booking.userId === user._id) {
+            res.send(booking);
+        } else {
+            res.status(403).send({
+                message: `This booking is not created by the current user`
+            })
+        }
+    }
+   } catch (ex) {
+    res.status(404).send({
+        message: `Booking with Id: ${req.params.id} does not exists`,
+    })
+   }
+}
+async function updateBooking(req, res) {  
+
+    if (req.userType === USERTYPES.ADMIN || req.userType === USERTYPES.CLINT){
+    const updateBooking = await Booking.findByIdAndUpdate(req.params.id, req.body);
+    res.send(updateBooking);
+ 
+     } else if (req.userType ===  USERTYPES.CUSTOMER) {
+        const userId = req.userId;
+        const user = await User.find({ userId: userId});
+     
+        try {
+        const booking = await Booking.findById(req.params.id);
+        if (booking.userId === user._id) {
+            const updateBooking = await Booking.findByIdAndUpdate(req.params.id, req.body);
+            res.send(updateBooking);
+        } else {
+            res.status(403).send({
+                message: `User can only updat a booking created by them`
+            })
+        }
+        } catch (ex) {
+            res.status(404).send({
+                message: `Booking with Id: ${req.params.id} does not exists`,
+            })
+        } 
+        
+    } 
+   }
+async function createBooking(req, rea) {
+    const booking = await Booking.create(req.body);
+    res.status(201).send(booking);
+}
 
 
 module.exports  = {
@@ -9,4 +76,4 @@ module.exports  = {
     getBookingById,
     updateBooking,
     createBooking,
-};
+}
--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -17,11 +17,66 @@ async function getTheatreById(req, res) {
     }
     }
 async function createTheatre(req, res) {
+const theater = req.body;
+try {
+    const newTheatre  = await Theatre.create(theater);
+    res.status(201).send(newTheatre);
+} catch (ex) {
+    res.status(400).send({
+        message: "Theatre body is invalid",
+    });
+}
     
 }
-async function updateTheatre(req, res) {}
-async function deleteTheatre(req, res) {}
-async function addMoviesToATheatre(req, res) {}
+
+async function updateTheatre(req, res) {
+    const id  = req.params.id;
+    try { 
+        Theatre.findById(id);
+    } catch (ex) {
+        res.status(404).send({
+            message: "Theater does not exiats",
+        })
+    }
+    const updateTheatre = await Theatre.findByIdAndUpdate(id, req.body);
+    res.send(updateTheatre);
+}
+
+
+async function deleteTheatre(req, res) {
+    const id = req.params.id;
+    try { 
+        Theatre.findById(id);
+    } catch (ex) {
+        res.status(404).send({
+            message: "Theater does not exiats",
+        })
+    }
+    await Theatre.findByIdAndDelete(id);
+}
+
+async function addMoviesToATheatre(req, res) {
+    const  moviesToBeAdded = req.body;
+    if (!Array.isArray(moviesToBeAdded)) {
+        res.status(400).send({
+            message: "Request body should be an array of movies ids",
+        });
+    }
+    const TheatreId = req.params.id;
+    try {
+        const theater = await Theatre.findById(TheatreId);
+        const existingMovies = theatre.movies;
+        const updateMovies = [...moviesToBeAdded, ...existingMovies];
+        theater.movies = updateMovies;
+        const updateTheatre = await Theatre.findByIdAndUpdate(TheatreIdm, theater);
+        res.status(200).send(updateTheatre);
+
+    } catch (ex) { 
+    res.status(404).send({
+        message: `Theatrea with ID: ${TheatreId} does not exist`,
+    });
+    }
+} 
 module.exports = {
     getAllTheatres,
     getTheatreById,
